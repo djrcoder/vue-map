7fdc22d1b398376690266acefc776ed0853af7e6
djrcoder <djrcode@gmail.com>
2020-05-29 19:25:48 +0900

    Added MdButton, MdCard, MdRipple

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
+
+import { MdButton, MdCard, MdRipple } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css'
 import App from './App.vue'
 import 'leaflet/dist/leaflet.css';
+
+Vue.use(MdButton)
+Vue.use(MdCard)
+Vue.use(MdRipple)
 
 
 Vue.config.productionTip = false
@@ -8,3 +16,5 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
